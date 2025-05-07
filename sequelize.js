e0dab6d70bb8060e17ddd8a54546800638f180aa
Ashish <ashishkap157@gmail.com>
2025-05-07 21:05:33 +0530

    Sequelize

--- sequelize.js
+++ sequelize.js
@@ -1,15 +1,52 @@
-const { Sequelize } = require("sequelize");
-const config =
-  require("./config/config.js")[process.env.NODE_ENV || "development"];
+// const { Sequelize } = require("sequelize");
+// const config =
+//   require("./config/config.js")[process.env.NODE_ENV || "development"];
 
-const sequelize = new Sequelize({
-  ...config,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+// const sequelize = new Sequelize({
+//   ...config,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// });
+
+
+const { Sequelize } = require("sequelize");
+const config = require("./config/config.js");
+const environment = process.env.NODE_ENV || "development";
+const envConfig = config[environment];
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    envConfig.database,
+    envConfig.username,
+    envConfig.password,
+    {
+      host: envConfig.host,
+      dialect: "postgres",
+      logging: false,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    }
+  );
+}
 
 module.exports = sequelize;
